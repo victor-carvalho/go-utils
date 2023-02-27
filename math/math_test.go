10e9ae6190bd72033a80b7a08363f32ef810c1d9
Victor Carvalho <carvalhogvm@gmail.com>
2023-02-27 16:33:39 -0300

    Refactor tests

--- math/math_test.go
+++ math/math_test.go
@@ -7,16 +7,22 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNextPowerofTwo_PowersOfTwo(t *testing.T) {
-	n := uint64(1)
-	for i := 0; i < 64; i++ {
-		value := n << i
-		require.Equal(t, value, NextPowerOfTwo64(value))
-	}
-}
+func TestNextPowerofTwo(t *testing.T) {
+	t.Run("returns 1 for 0", func(t *testing.T) {
+		require.Equal(t, uint(1), NextPowerOfTwo(0))
+	})
 
-func TestNextPowerofTwo_General(t *testing.T) {
-	quick.Check(func(n uint) bool {
-		return IsPowerOfTwo(NextPowerOfTwo(n))
-	}, nil)
+	t.Run("returns same number if it's a power of two", func(t *testing.T) {
+		n := uint64(1)
+		for i := 0; i < 64; i++ {
+			value := n << i
+			require.Equal(t, value, NextPowerOfTwo64(value))
+		}
+	})
+
+	t.Run("always returns a power of two", func(t *testing.T) {
+		quick.Check(func(n uint) bool {
+			return IsPowerOfTwo(NextPowerOfTwo(n))
+		}, nil)
+	})
 }
